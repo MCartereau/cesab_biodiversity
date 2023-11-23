131c4e96f5ef2c8d232a7cb5cf0962b66a67e195
TerenceLegrand <legrandterence@gmail.com>
2023-11-23 17:08:19 +0100

    function path spatial

--- R/path_spat_grid.R
+++ R/path_spat_grid.R
@@ -7,6 +7,6 @@
 #'
 #' @examples
 path_spat_grid <- function(filename) {
-  path <- here::here("data", "raw-data",filename)
+  path <- here::here("data", "raw-data","50x50_EUROMEDIT",filename)
   return(path)
 }
