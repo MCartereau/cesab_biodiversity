9ad30946d5fd088836156cbb569d81b9256cac88
Ellis Franklin <ellisfranklin5@gmail.com>
2023-11-24 11:23:03 +0100

    FINAL MASSIVE COMMIT SHIIIIT

--- R/phy_rich.R
+++ R/phy_rich.R
@@ -14,7 +14,7 @@ phy_rich <- function(data, phylo_data, taxon_id) {
   picante::pd(data, phylo_data[[taxon_id]], include.root=FALSE) |>
     dplyr::bind_cols(data[,1]) |>
     dplyr::rename(Idgrid = ...3) |>
-    dplyr::mutate(Variable = "aphylotree") |>
+    dplyr::mutate(Variable = paste("aphylo", taxon_id, sep="")) |>
     dplyr::mutate(Dimension = "Alpha") |>
     dplyr::mutate(Facet = "Phylogenetic") |>
     dplyr::mutate(Taxon = taxon_id) |>
--- analyses/pipeline.R
+++ analyses/pipeline.R
@@ -21,15 +21,23 @@ tar_target(load_spat, load_spat_grid(pathspatgrid)),
 
 ## 3 - Format data ------------------------------------------------------
 
-# 3.1 - Select occurrences in a 50x50 grid in the EUROMEDIT region
+# 3.1 - Transform phylo data : index multifile as taxon names
+
+tar_target(id_phy, id_phylo(load_phylogeny, "load_phylogeny_mod.tree")),
+
+# 3.2 - Select occurrences in a 50x50 grid in the EUROMEDIT region
 tar_target(select_occ, occ_select(load_occ, "50x50", "EUROMEDIT")),
 
 # 3.2 - Format species-site matrix
-tar_target(format_sp_tree_site, sp_site_mat(select_occ, "Trees")),
-tar_target(format_sp_animal_site, sp_site_mat(select_occ, "Birds")),
+tar_target(format_sp_tree_func_site, sp_site_mat_func(select_occ, "Trees")),
+tar_target(format_sp_animal_func_site, sp_site_mat_func(select_occ, "Birds")),
+tar_target(format_sp_tree_phylo_site, sp_site_mat_phylo(select_occ, "Trees")),
+tar_target(format_sp_animal_phylo_site, sp_site_mat_phylo(select_occ, "Birds")),
+
 
 ## 4 - Functional traits selection ---------------------------------------
-tar_target(select_traits, traits_select(load_traits, c("Mass", "Clutch_MEAN", "Beak.Depth", "Beak.Width", "Tail.Length"))),
+tar_target(select_animal_traits, traits_select(load_traits, c("Mass", "Clutch_MEAN", "Beak.Depth", "Beak.Width", "Tail.Length"))),
+tar_target(select_tree_traits, traits_select(load_traits, c("LA", "SLA", "SeedMass", "StemSpecDens", "HeightMax"))),
 
 ## 5 - Compute indices --
 # 
@@ -37,18 +45,18 @@ tar_target(select_traits, traits_select(load_traits, c("Mass", "Clutch_MEAN", "B
 tar_target(comp_sp_rich, sp_rich(select_occ)),
 
 # 5.2 - Phylogenetic richness
-tar_target(comp_tree_phylo_rich, phy_rich(, load_phylogeny, taxon_id = "Trees")),
-tar_target(comp_animal_phylo_rich, phy_rich(, load_phylogeny, "Birds")),
+tar_target(comp_phylo_tree_rich, phy_rich(format_sp_tree_phylo_site, id_phy, taxon_id = "Trees")),
+tar_target(comp_phylo_animal_rich, phy_rich(format_sp_animal_phylo_site, id_phy, taxon_id = "Birds")),
 
 # 5.3 - Functional richness
-tar_target(comp_tree_func_rich, func_rich(select_traits, format_sp_tree_site, taxon_id = "Trees")),
-tar_target(comp_animal_func_rich, func_rich(select_traits, format_sp_animal_site, taxon_id = "Birds")),
+tar_target(comp_func_tree_rich, func_rich(select_tree_traits, format_sp_tree_func_site, taxon_id = "Trees")),
+tar_target(comp_func_animal_rich, func_rich(select_animal_traits, format_sp_animal_func_site, taxon_id = "Birds")),
 
 ## 5 - Combine richness indices --------------------------------------------
  
 tar_target(comb_ind_rich, ind_rich(comp_sp_rich, 
-                                   comp_animal_func_rich, comp_tree_func_rich, 
-                                   comp_animal_phylo_rich, comp_tree_phylo_rich)),
+                                   comp_func_animal_rich, comp_func_tree_rich, 
+                                   comp_phylo_animal_rich, comp_phylo_tree_rich)),
  
  
 ## Plot
